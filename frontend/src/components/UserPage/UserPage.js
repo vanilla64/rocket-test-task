d8afc63e39371cc6d07056f4ea9f4fe67b3be561
vanilla64 <62620124+vanilla64@users.noreply.github.com>
2021-05-19 10:20:29 +0300

    update user page

--- frontend/src/components/UserPage/UserPage.js
+++ frontend/src/components/UserPage/UserPage.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import './UserPage.sass'
 
 function UserPage(props) {
   return (
     <section className="user-page">
-      <h1>User Name</h1>
+      <img className="user-page__img" />
+      <h1 className="user-page__title">User Name</h1>
+      <p className="user-page__mail">mail@ex.com</p>
     </section>
-  );
+  )
 }
 
 export default UserPage
